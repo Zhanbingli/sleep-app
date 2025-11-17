f1a51841894e71181e301fcd0ebc7f9ffc82a365
zhanbingli <3163525375@qq.com>
2025-11-17 20:40:46 +0800

    Initial Commit

---
+++ sleep/sleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepApp.swift
+//  sleep
+//
+//  Created by lizhanbing12 on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct sleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sleepTests/sleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  sleepTests.swift
+//  sleepTests
+//
+//  Created by lizhanbing12 on 17/11/25.
+//
+
+import Testing
+@testable import sleep
+
+struct sleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
